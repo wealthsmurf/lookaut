e4737562e69ae6609266938b24dedc4a28893067
wealthsmurf <98888433+wealthsmurf@users.noreply.github.com>
2026-01-21 23:06:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+import os
 import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+async def read_index():
+    return FileResponse('index.html')
+
+@app.get("/Bgggm.mp3")
+async def get_audio():
+    return FileResponse('Bgggm.mp3')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    # Railway가 제공하는 포트를 읽어옴, 기본값은 8080
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
