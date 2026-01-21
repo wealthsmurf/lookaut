10841063e42e79502baeb1034cd5e72547597f52
wealthsmurf <98888433+wealthsmurf@users.noreply.github.com>
2026-01-21 22:49:08 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
